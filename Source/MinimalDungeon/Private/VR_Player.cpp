842f05424fad81d7a1ba49f4d4de29df7da7e2d5
noramlnine <codejamgg@gmail.com>
2023-01-31 18:19:38 +0900

    KDH_FirstSetting

--- Source/MinimalDungeon/Private/VR_Player.cpp
+++ Source/MinimalDungeon/Private/VR_Player.cpp
@@ -2,6 +2,12 @@
 
 
 #include "VR_Player.h"
+#include <MotionControllerComponent.h>
+#include <Camera/CameraComponent.h>
+#include <Engine/StaticMesh.h>
+#include <Components/StaticMeshComponent.h>
+#include <HeadMountedDisplayFunctionLibrary.h>
+#include <Components/TextRenderComponent.h>
 
 // Sets default values
 AVR_Player::AVR_Player()
@@ -9,12 +15,72 @@ AVR_Player::AVR_Player()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	cameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	cameraComponent->SetupAttachment(RootComponent);
+
+	motionControllerRight = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("motionControllerRight"));
+	motionControllerLeft = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("motionControllerLeft"));
+	motionControllerRight->SetupAttachment(RootComponent);
+	motionControllerLeft->SetupAttachment(RootComponent);
+	motionControllerLeft->MotionSource = "Left";
+	motionControllerRight->MotionSource = "Right";
+
+	rightHand = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("rightHand"));
+	rightHand->SetupAttachment(motionControllerRight);
+	rightHand->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	leftHand = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("leftHand"));
+	leftHand->SetupAttachment(motionControllerLeft);
+	leftHand->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+
+	HMD = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("HMD"));
+	HMD->SetupAttachment(cameraComponent);
+	HMD->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	rightLog = CreateDefaultSubobject<UTextRenderComponent>(TEXT("RightLogText"));
+	rightLog->SetupAttachment(motionControllerRight);
+	rightLog->SetRelativeRotation(FRotator(135, 0, 180));
+	rightLog->SetRelativeLocation(FVector(0, 0, 10));
+	rightLog->SetTextRenderColor(FColor::Yellow);
+	rightLog->SetHorizontalAlignment(EHTA_Center);
+	rightLog->SetHorizontalAlignment(EHTA_Center);
+
+	leftLog = CreateDefaultSubobject<UTextRenderComponent>(TEXT("LeftLogText"));
+	leftLog->SetupAttachment(motionControllerLeft);
+	leftLog->SetRelativeRotation(FRotator(135, 0, 180));
+	leftLog->SetRelativeLocation(FVector(0, 0, 10));
+	leftLog->SetTextRenderColor(FColor::Yellow);
+	leftLog->SetHorizontalAlignment(EHTA_Center);
+
+
+
+	ConstructorHelpers::FObjectFinder<UStaticMesh> tempRightMesh(TEXT("StaticMesh'/OpenXR/Devices/OculusTouch_v3/Right/right_OculusTouch_v3Controller.right_OculusTouch_v3Controller'"));
+	if (tempRightMesh.Succeeded())
+	{
+		rightHand->SetStaticMesh(tempRightMesh.Object);
+	}
+
+	ConstructorHelpers::FObjectFinder<UStaticMesh> tempLeftMesh(TEXT("StaticMesh'/OpenXR/Devices/OculusTouch_v3/Left/left_OculusTouch_v3Controller.left_OculusTouch_v3Controller'"));
+	if (tempLeftMesh.Succeeded())
+	{
+		leftHand->SetStaticMesh(tempLeftMesh.Object);
+	}
+
+	ConstructorHelpers::FObjectFinder<UStaticMesh> tempHMDMesh(TEXT("StaticMesh'/Engine/VREditor/Devices/Generic/GenericHMD.GenericHMD'"));
+	if (tempHMDMesh.Succeeded())
+	{
+		HMD->SetStaticMesh(tempHMDMesh.Object);
+	}
 }
 
 // Called when the game starts or when spawned
 void AVR_Player::BeginPlay()
 {
 	Super::BeginPlay();
+
+	// 헤드 장비의 기준 위치를 설정한다.
+	UHeadMountedDisplayFunctionLibrary::SetTrackingOrigin(trackingOrigin.GetValue());
 	
 }
 
@@ -31,4 +97,3 @@ void AVR_Player::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 }
-
