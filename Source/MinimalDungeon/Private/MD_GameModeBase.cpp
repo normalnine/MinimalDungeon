ca2c1670ae6f27fa1341333a48acb55753e93268
noramlnine <codejamgg@gmail.com>
2023-02-02 15:57:09 +0900

    KDH_Folder

--- Source/MinimalDungeon/Private/MD_GameModeBase.cpp
+++ Source/MinimalDungeon/Private/MD_GameModeBase.cpp
@@ -5,7 +5,7 @@
 
 AMD_GameModeBase::AMD_GameModeBase()
 {
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("Blueprint'/Game/KDH/Bluprints/BP_VR_Player.BP_VR_Player_C'"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("Blueprint'/Game/KDH/Blueprints/BP_VR_Player.BP_VR_Player_C'"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
